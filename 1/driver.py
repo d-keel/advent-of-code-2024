3c13950c6cb055eeb734e28b62ebb0269e377300
Dalton <daltonkeel02@gmail.com>
2024-12-01 14:48:14 -0500

    Solved part 2 of day 1.

--- 1/driver.py
+++ 1/driver.py
@@ -7,27 +7,40 @@ def readFile(filePath: str) -> tuple[list[int], list[int]]:
         for line in lines:
             line = line.strip()
             splitLine: list[str] = line.split(sep='   ')
-            left.append(int(splitLine[0]))
-            right.append(int(splitLine[1])) 
+            leftNum, rightNum = int(splitLine[0]), int(splitLine[1])
+            left.append(leftNum)
+            right.append(rightNum)
     
     return left, right
 
 def sortValues(left: list[int], right: list[int]) -> tuple[list[int], list[int]]:
     return sorted(left), sorted(right)
 
-def calculateDifference(left: list[int], right: list[int]) -> int:
+def calculateDifference(left: list[int], right: list[int]) -> tuple[int, int]:
     total: int = 0
+    similarity: int = 0
+
+    myDict: dict[int, int] = {}
 
     for i, v in enumerate(left):
+        myDict[v] = myDict.get(v, 0)
         total += abs(v - right[i])
 
-    return total
+    for v in right:
+        if v in myDict:
+            myDict[v] += 1
+
+    for k, v in myDict.items():
+        score: int = k * v
+        similarity += score
+
+    return total, similarity
 
 def main() -> None:
     left, right = readFile('input.txt')
     left, right = sortValues(left, right)
-    total: int = calculateDifference(left, right)
-    print(total)
+    total, similarity = calculateDifference(left, right)
+    print(total, similarity)
 
 
 if __name__ == "__main__":
